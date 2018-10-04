25cee7bf6ec4f4c8c160cb42711f640ec654854d
Julian Guarin <julian.guarin@globant.com>
2018-10-04 14:42:07 -0500

    WIP

---
+++ glcollader.cpp
@@ -0,0 +1,31 @@
+
+#include "collader.h"
+
+#include <vector>
+#include <map>
+
+namespace GTech {
+
+    struct NodeAccess{
+
+        ssize_t bytesPerElement;
+        unsigned int elementCount;
+    
+    };
+
+    struct GlColladerScene {
+
+        std::map<std::string, NodeAccess> nodeAccess;
+        
+    private:
+        /**
+         * Vertex data
+         */
+        std::vector<float> _vertexdata_;
+        std::vector<float> _normaldata_;
+        std::vector<float> _texcrddata_;
+
+
+    };
+
+}    
