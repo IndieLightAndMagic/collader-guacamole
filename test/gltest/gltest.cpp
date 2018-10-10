c4802acabfdd25aae3e029809f92d7079e9f1474
Julian Guarin <julian.guarin@globant.com>
2018-10-10 13:58:07 -0500

    WIP on VBO and VAOs

--- test/gltest/gltest.cpp
+++ test/gltest/gltest.cpp
@@ -97,83 +97,38 @@ std::shared_ptr<VtxEntry> GetVtxs(const GTech::Mesh& mesh){
 }
 void drawscene(const GTech::Scene& rScene)
 {
-    int i; /* Simple iterator */    GLuint vao, vbo[2]; /* Create handles for our Vertex Array Object and two Vertex Buffer Objects */    int isCompiled_VS, isCompiled_FS;    int IsLinked;    int maxLength;    char *vertexInfoLog;    char *fragmentInfoLog;    char *shaderProgramInfoLog;
-    /* We're going to create a simple diamond made from lines */
-    const GLfloat diamond[4][2] = {
-    {  0.0,  1.0  }, /* Top point */
-    {  1.0,  0.0  }, /* Right point */
-    {  0.0, -1.0  }, /* Bottom point */
-    { -1.0,  0.0  } }; /* Left point */
-    const GLfloat colors[4][3] = {
-    {  1.0,  0.0,  0.0  }, /* Red */
-    {  0.0,  1.0,  0.0  }, /* Green */
-    {  0.0,  0.0,  1.0  }, /* Blue */
-    {  1.0,  1.0,  1.0  } }; /* White */
-
-    /* These pointers will receive the contents of our shader source code files */
     
-    /* These are handles used to reference the shaders */
-    GLuint vertexshader, fragmentshader;
-
-    /* Allocate and assign a Vertex Array Object to our handle */
-    glGenVertexArrays(1, &vao);
-
-    /* Bind our Vertex Array Object as the current used object */
-    glBindVertexArray(vao);
-
-    /* Allocate and assign two Vertex Buffer Objects to our handle */
-    glGenBuffers(2, vbo);
-
-    /* Bind our first VBO as being the active buffer and storing vertex attributes (coordinates) */
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-
-    /* Copy the vertex data from diamond to our buffer */
-    /* 8 * sizeof(GLfloat) is the size of the diamond array, since it contains 8 GLfloat values */
-    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), diamond, GL_STATIC_DRAW);
-
-    /* Specify that our coordinate data is going into attribute index 0, and contains two floats per vertex */
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    /* Enable attribute index 0 as being used */
-    glEnableVertexAttribArray(0);
-
-    /* Bind our second VBO as being the active buffer and storing vertex attributes (colors) */
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-
-    /* Copy the color data from colors to our buffer */
-    /* 12 * sizeof(GLfloat) is the size of the colors array, since it contains 12 GLfloat values */
-    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
-
-    /* Specify that our color data is going into attribute index 1, and contains three floats per vertex */
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-    /* Enable attribute index 1 as being used */
-    glEnableVertexAttribArray(1);
-
     /* Get GPU Program */
     auto shaderprogram = GetShaders();
     shaderprogram.use();
     
-    
+    std::map<std::string, std::shared_ptr<VtxEntry>> vtxs{};
+
+    for (auto& meshname_mesh : rScene.meshes){
+
+        auto meshname = meshname_mesh.first;
+        auto mesh     = meshname_mesh.second;
+        
+    }
 
     
     /* Loop our display increasing the number of shown vertexes each time.
      * Start with 2 vertexes (a line) and increase to 3 (a triangle) and 4 (a diamond) */
-    for (i=2; i <= 4; i++)
-    {
+    //for (i=2; i <= 4; i++)
+    //{
         /* Make our background black */
-        glClearColor(0.0, 0.0, 0.0, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
+    //  glClearColor(0.0, 0.0, 0.0, 1.0);
+    //  glClear(GL_COLOR_BUFFER_BIT);
 
         /* Invoke glDrawArrays telling that our data is a line loop and we want to draw 2-4 vertexes */
-        glDrawArrays(GL_LINE_LOOP, 0, i);
+    //  glDrawArrays(GL_LINE_LOOP, 0, i);
 
         /* Swap our buffers to make our changes visible */
-        GTech::SDLGlSwapWindow();
+    //  GTech::SDLGlSwapWindow();
 
         /* Sleep for 2 seconds */
-        SDL_Delay(2000);
-    }
+    //  SDL_Delay(2000);
+    //}
 
     /* Cleanup all the things we bound and allocated */
     //glUseProgram(0);
